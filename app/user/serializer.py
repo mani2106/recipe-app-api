dd9ea3a1bb048cf1d605bafd0a3f8987bd7cd2dc
mani2106 <manimaran_p@outlook.com>
2024-01-30 21:54:41 +0530

    Implement manage_user api

--- app/user/serializer.py
+++ app/user/serializer.py
@@ -23,6 +23,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return user with encrypted pwd"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        passw = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if passw:
+            user.set_password(passw)
+            user.save()
+
+        return user
+
 
 class UserAuthTokenSerializer(serializers.Serializer):
     """Serializer for user auth token"""
--- app/user/urls.py
+++ app/user/urls.py
@@ -4,7 +4,7 @@ Mappings for user api
 from django.urls import path
 
 # from user import views
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = 'user'
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path('create/', view=CreateUserView.as_view(),
          # name is used in reverse lookup, check test_user_api.py
          name='create'),
-    path('token/', view=CreateTokenView.as_view(), name='token')
+    path('token/', view=CreateTokenView.as_view(), name='token'),
+    path('me/', view=ManageUserView.as_view(), name='me')
 ]
