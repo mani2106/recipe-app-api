b72ed9da008d8d382ec0755f14f64df913fbc424
mani2106 <manimaran_p@outlook.com>
2024-02-06 21:01:28 +0530

    Add recipe api logic

---
+++ app/recipe/serializers.py
@@ -0,0 +1,22 @@
+"""
+Serializers for Recipes
+"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for Recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_min', 'price', 'link']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for Recipe Details"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields = ['desc']
